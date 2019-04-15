2df524fbd765ee2dc324748cc4c37164ae92a486
Hayawi <yahya.ismail@hotmail.ca>
2019-04-15 05:31:49 -0400

    Finished JSON for OrderService

--- src/bean/POWrapperBean.java
+++ src/bean/POWrapperBean.java
@@ -2,10 +2,13 @@ package bean;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="purchaseOrder")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class POWrapperBean {
 	@XmlElement(name = "shipTo")
 	private AddressBean addressToShip;
@@ -19,6 +22,22 @@ public class POWrapperBean {
 	
 	public POWrapperBean(AddressBean addressToShip, List<POItemBean> items) {
 		this.addressToShip = addressToShip;
+		this.items = items;
+	}
+	
+	public AddressBean getAddressToShip() {
+		return addressToShip;
+	}
+
+	public void setAddressToShip(AddressBean addressToShip) {
+		this.addressToShip = addressToShip;
+	}
+
+	public List<POItemBean> getItems() {
+		return items;
+	}
+
+	public void setItems(List<POItemBean> items) {
 		this.items = items;
 	}
 	
