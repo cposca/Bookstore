f6e9972be653af75eb02768f4e00cb836e7745d7
cposca <cposca3@gmail.com>
2019-04-15 04:42:18 -0400

    Pls work man

--- src/ctrl/SignupServlet.java
+++ src/ctrl/SignupServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
+
 import bean.LoginBean;
 import dao.AddressDAO;
 import dao.LoginDAO;
@@ -25,34 +27,31 @@ public class SignupServlet extends HttpServlet {
 	private String signupPage = "Signup.jspx";
 	private String successPage = "Success.jspx";
 	private String mainServlet = "Store";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
-	 * @see HttpServlet#HttpServlet()
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	public SignupServlet() {
-		super();
-		// TODO Auto-generated constructor stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doPost(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getSession().removeAttribute("error");
-		if (!(request.getParameter("success") == null)) {
+		if(!(request.getParameter("success") == null)) {
 			response.sendRedirect(mainServlet);
-		} else if (!(request.getParameter("signup") == null)) {
+		}
+		else if(!(request.getParameter("signup") == null)) {
 			String username = request.getParameter("username");
 			String password = request.getParameter("password");
 			String repeatPassword = request.getParameter("repeatPassword");
@@ -65,67 +64,70 @@ public class SignupServlet extends HttpServlet {
 			String phone = request.getParameter("phone");
 			LoginDAO loginDAO = new LoginDAO();
 			List<LoginBean> list;
-			try {
-				list = loginDAO.retrieve(username);
-				if (list.isEmpty()) {
-					if (username.isEmpty() || password.isEmpty() || fName.isEmpty() || lName.isEmpty()
-							|| street.isEmpty() || country.isEmpty() || province.isEmpty() || zip.isEmpty()
-							|| phone.isEmpty()) {
-						request.getSession().setAttribute("error", "All fields need to be full");
-					} else {
-						if (password == repeatPassword) {
-							if (password.length() >= 6) {
-								SecureRandom random = new SecureRandom();
-								byte[] salt = new byte[64];
-								random.nextBytes(salt);
-								String encryptedPass = hashToString(sha256(password, salt));
-								loginDAO.create(username, encryptedPass, hashToString(salt));
-								list = loginDAO.retrieve(username);
-								LoginBean bean = list.get(0);
-								AddressDAO addDAO = new AddressDAO();
-								addDAO.create(bean.getId(), street, province, country, zip, phone);
-								UserDAO userDAO = new UserDAO();
-								userDAO.create(bean.getId(), fName, lName);
-								request.getRequestDispatcher(successPage).forward(request, response);
-							} else {
-								request.getSession().setAttribute("error", "The password is too short");
-							}
-						} else {
-							request.getSession().setAttribute("error", "The passwords do not match");
-						}
-					}
+				if(username.isEmpty() || password.isEmpty() || fName.isEmpty() || lName.isEmpty() || street.isEmpty() || country.isEmpty() || province.isEmpty() || zip.isEmpty() || phone.isEmpty()) {
+					request.getSession().setAttribute("error", "All fields need to be full");
+					request.getRequestDispatcher(signupPage).forward(request,response);
 				} else {
-					request.getSession().setAttribute("error", "The username already exists!");
+					try {
+						list = loginDAO.retrieve(username);
+						if(list.isEmpty()) {
+							if(password.equals(repeatPassword)) {
+								if(password.length() >= 6) {
+									SecureRandom random = new SecureRandom();
+									byte[] salt = new byte[64];
+									random.nextBytes(salt);
+									String encryptedPass = hashToString(sha256(password,DatatypeConverter.printBase64Binary(salt)));
+									loginDAO.create(username, encryptedPass, DatatypeConverter.printBase64Binary(salt));
+									list = loginDAO.retrieve(username);
+									LoginBean bean = list.get(0);
+									AddressDAO addDAO = new AddressDAO();
+									addDAO.create(bean.getId(), street, province, country, zip, phone);
+									UserDAO userDAO = new UserDAO();
+									userDAO.create(bean.getId(), fName, lName);
+									request.getRequestDispatcher(successPage).forward(request,response);
+								}else {
+									request.getSession().setAttribute("error", "The password is too short");
+									request.getRequestDispatcher(signupPage).forward(request,response);
+								}
+							}else {
+								request.getSession().setAttribute("error", "The passwords do not match");
+								request.getRequestDispatcher(signupPage).forward(request,response);
+							}
+						}else {
+						request.getSession().setAttribute("error", "The username already exists!");
+						request.getRequestDispatcher(signupPage).forward(request,response);
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 				}
-			} catch (SQLException e) {
-				e.printStackTrace();
 			}
-		}
-		request.getRequestDispatcher(signupPage).forward(request, response);
-	}
-
-	private static byte[] sha256(String base, byte[] salt) {
-		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			digest.update(base.getBytes("UTF-8"));
-			digest.update(salt);
-			return digest.digest();
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
+		else {
+			request.getRequestDispatcher(signupPage).forward(request,response);
 		}
 	}
 
+	private static byte[] sha256(String base, String salt) {
+	    try{
+	        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	        digest.update(base.getBytes("UTF-8"));
+	        digest.update(salt.getBytes("UTF-8"));
+	        return digest.digest();
+	    } catch(Exception ex){
+	       throw new RuntimeException(ex);
+	    }
+	}
+	
 	private String hashToString(byte[] hash) {
 		StringBuffer hexString = new StringBuffer();
 
-		for (int i = 0; i < hash.length; i++) {
-			String hex = Integer.toHexString(0xff & hash[i]);
-			if (hex.length() == 1)
-				hexString.append('0');
-			hexString.append(hex);
-		}
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
 
-		return hexString.toString();
+        return hexString.toString();
 	}
 
 }
