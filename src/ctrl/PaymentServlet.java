13f82bfd19f7a4d342c8671f31dfa436c65ce7d8
Artem Solovey <artem95@my.yorku.ca>
2019-04-14 22:13:04 -0400

    Created PaymentPage and made it so login button shows only when loggedin (#25)

---
+++ src/ctrl/PaymentServlet.java
@@ -0,0 +1,46 @@
+package ctrl;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShoppingCart
+ */
+@WebServlet(urlPatterns = { "/Payment", "/payment" })
+public class PaymentServlet extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PaymentServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/PaymentPage.jspx").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
